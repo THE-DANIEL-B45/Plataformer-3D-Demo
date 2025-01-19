c4df7d10be32d8fdd6793aa002ffb4af39ce597a
THE-DANIEL-B45 <danielborgeslazzarini0@gmail.com>
2025-01-19 16:50:02 -0300

    Movimento do Herói

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Animator animator;
+
+    public CharacterController characterController;
+    public float speed = 1f;
+    public float turnSpeed = 1f;
+    public float gravity = -9.8f;
+
+    private float vSpeed = 0f;
+
+    public float jumpSpeed = 15f;
+
+    public KeyCode jumpKeyCode = KeyCode.Space;
+
+    [Header("Run Setup")]
+    public KeyCode keyRun = KeyCode.LeftShift;
+    public float speedRun = 1.5f;
+
+    private void Update()
+    {
+        transform.Rotate(0, Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime, 0);
+
+        var inputAxisVertical = Input.GetAxis("Vertical");
+        var speedVector = transform.forward * inputAxisVertical * speed;
+
+        if(characterController.isGrounded)
+        {
+            vSpeed = 0f;
+            if(Input.GetKeyDown(jumpKeyCode))
+            {
+                vSpeed = jumpSpeed;
+            }
+        }
+
+        vSpeed -= gravity * Time.deltaTime;
+        speedVector.y = vSpeed;
+
+
+        var isWalking = inputAxisVertical != 0;
+        if(isWalking)
+        {
+            if(Input.GetKey(keyRun))
+            {
+                speedVector *= speedRun;
+                animator.speed = speedRun;
+            }
+        }
+
+        characterController.Move(speedVector * Time.deltaTime);
+
+        animator.SetBool("Run", inputAxisVertical != 0);
+    }
+}
