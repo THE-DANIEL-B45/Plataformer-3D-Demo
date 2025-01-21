7458b6baa56ec8da706e9ac21b6e26506a9fc6a0
THE-DANIEL-B45 <danielborgeslazzarini0@gmail.com>
2025-01-21 18:48:43 -0300

    Diferentes armas

---
+++ Assets/Scripts/Gun/GunShootLimit.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GunShootLimit : GunBase
+{
+    public List<UIGunUpdater> UIGunUpdaterList;
+
+    public float maxShoot = 5f;
+    public float timeToRecharge = 1f;
+
+    protected float _currentShots;
+    protected bool _recharging = false;
+
+    private void Awake()
+    {
+        GetAllUis();
+    }
+
+    protected override IEnumerator ShootCoroutine()
+    {
+        if(_recharging) yield break;
+
+        while (true)
+        {
+            if(_currentShots < maxShoot)
+            {
+                Shoot();
+                _currentShots++;
+                CheckRecharge();
+                UpdateUI();
+                yield return new WaitForSeconds(timeBetweenShoot);
+            }
+        }
+    }
+
+    protected void CheckRecharge()
+    {
+        if(_currentShots>= maxShoot)
+        {
+            StopShoot();
+            StartRecharge();
+        }
+    }
+
+    protected void StartRecharge()
+    {
+        _recharging = true;
+        StartCoroutine(RechargeCoroutine());
+    }
+
+    protected IEnumerator RechargeCoroutine()
+    {
+        float time = 0;
+        while(time< timeToRecharge)
+        {
+            time += Time.deltaTime;
+            UIGunUpdaterList.ForEach(i => i.UpdateValue(time/timeToRecharge));
+            yield return new WaitForEndOfFrame();
+        }
+        _currentShots = 0;
+        _recharging = false;
+    }
+
+    protected void UpdateUI()
+    {
+        UIGunUpdaterList.ForEach(i => i.UpdateValue(maxShoot, _currentShots));
+    }
+
+    protected void GetAllUis()
+    {
+        UIGunUpdaterList = GameObject.FindObjectsOfType<UIGunUpdater>().ToList();
+    }
+}
---
+++ Assets/Scripts/Player/PlayerAbilityShoot.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerAbilityShoot : PlayerAbilityBase
+{
+    public GunBase gun1;
+    public GunBase gun2;
+    public Transform gunPosition;
+    private GunBase _currentGun;
+
+    protected override void Init()
+    {
+        base.Init();
+
+        ChangeGun(gun1);
+
+        inputs.Gameplay.Shoot.performed += ctx => StartShoot();
+        inputs.Gameplay.Shoot.canceled += ctx => CancelShoot();
+
+        inputs.Gameplay.Gun1.performed += ctx => ChangeGun(gun1);
+        inputs.Gameplay.Gun2.performed += ctx => ChangeGun(gun2);
+    }
+
+    private void ChangeGun(GunBase gun)
+    {
+        if(_currentGun != null) Destroy(_currentGun.gameObject);
+
+        _currentGun = Instantiate(gun, gunPosition);
+
+        _currentGun.transform.localPosition = _currentGun.transform.localEulerAngles = Vector3.zero;
+    }
+
+    private void StartShoot()
+    {
+        _currentGun.StartShoot();
+        Debug.Log("StartShoot");
+    }
+
+    private void CancelShoot()
+    {
+        Debug.Log("CancelShoot");
+        _currentGun.StopShoot();
+    }
+}
